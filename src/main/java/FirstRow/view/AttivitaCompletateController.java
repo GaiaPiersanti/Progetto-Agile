ad3c003668403743d19a07f5b35262d2272b7bf4
GaiaPiersanti <135056936+GaiaPiersanti@users.noreply.github.com>
2025-02-20 09:54:09 +0100

    elenco attività completate con tasti funzionanti

--- src/main/java/FirstRow/view/AttivitaCompletateController.java
+++ src/main/java/FirstRow/view/AttivitaCompletateController.java
@@ -1,6 +1,7 @@
 package FirstRow.view;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.Date;
@@ -10,16 +11,21 @@ import java.sql.Statement;
 import java.util.ResourceBundle;
 
 import FirstRow.Database;
+import FirstRow.MainFx;
 import FirstRow.Model.Attivita;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class AttivitaCompletateController implements Initializable {
@@ -33,6 +39,8 @@ public class AttivitaCompletateController implements Initializable {
 	private TableColumn<Attivita,Date> ColScadenza;
 	@FXML
 	private TableColumn<Attivita,String> ColPriorita;
+
+	//icone sul bordo
     @FXML
 	private ImageView bannerImageIC;
 	@FXML
@@ -67,6 +75,7 @@ public class AttivitaCompletateController implements Initializable {
         ColScadenza.setCellValueFactory(new PropertyValueFactory<Attivita,Date>("Scadenza"));
         ColPriorita.setCellValueFactory(new PropertyValueFactory<Attivita,String>("Priorita"));
 
+		//riempimento della tabella
         Connection con = Database.collegamento();
 		ObservableList<Attivita> list = FXCollections.observableArrayList();
 		if (con == null) {
@@ -87,11 +96,36 @@ public class AttivitaCompletateController implements Initializable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        TabellaAttivita.setItems(list);
+        TabellaAttivita.setItems(list); // il list dentro il while prende tutti gli elementi prese dalla select e poi lo uso per riempire la tabella
     }
 	
+	//passaggio dello stage dalla pagina precedente a questa
 	public void setDialogStage(Stage dialogStage) {
         this.StageCompl = dialogStage;
+    }
+
+	//metodi per far funzionare i bottoni e passare alle altre pagine 
+	public void elencoAttivita(MouseEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FirstRow/view/ElencoAttivita.fxml"));
+        Parent root = loader.load();
+        
+        ElencoAttivitaController controller = loader.getController();
+        controller.setDialogStage(StageCompl);
+        
+        StageCompl.setTitle("Elenco Attvità");
+        StageCompl.setScene(new Scene(root));
+        StageCompl.show();
+    }
+    
+    public void Calendario(MouseEvent event) throws IOException{
+    	FXMLLoader loader = new FXMLLoader(MainFx.class.getResource("/FirstRow/view/Calendar.fxml"));
+        Parent root = loader.load();
+        
+        CalendarController controller = loader.getController();
+        controller.setStage(StageCompl);
+        StageCompl.setTitle("Calendar");
+        StageCompl.setScene(new Scene(root));
+		StageCompl.show();
     }
 }
 
